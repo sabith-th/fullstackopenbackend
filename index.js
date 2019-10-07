1ff997eadb74ba73b57b0915e30bcb0305cf5637
SabithHaneef <sabith.th.07@gmail.com>
2019-10-07 16:58:38 +0530

    completed exercise 3.1 to 3.6

---
+++ index.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const PORT = 3001;
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4
+  }
+];
+
+app.use(bodyParser.json());
+
+const generateId = () => Math.floor(Math.random() * Math.floor(100000));
+
+const findDuplicate = name =>
+  persons.find(person => person.name.toLowerCase() === name.toLowerCase());
+
+app.get("/info", (req, res) => {
+  res.send(`
+    <p>Phonebook has info on ${persons.length} people</p>
+    <p>${new Date()}</p>
+  `);
+});
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find(person => person.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter(person => person.id !== id);
+  res.status(204).end();
+});
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+
+  if (!body.name) {
+    return res.status(400).json({
+      error: "Contact name is missing"
+    });
+  } else if (!body.number) {
+    return res.status(400).json({
+      error: "Contact number is missing"
+    });
+  }
+
+  if (findDuplicate(body.name)) {
+    return res.status(400).json({
+      error: "Contact name must be unique"
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  };
+
+  persons = persons.concat(person);
+
+  res.json(person);
+});
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
